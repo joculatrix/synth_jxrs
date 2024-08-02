cf97a82a8efb0812579cc40286130a136a993ca8
Sloane Vaznova <sloane.vaznova@gmail.com>
2024-08-02 17:23:08 -0500

    modified synthesizer to continue running until exit input is received from user

--- src/message.rs
+++ src/message.rs
@@ -3,6 +3,7 @@ use tokio::sync::broadcast::{Sender, Receiver};
 #[derive(Clone)]
 pub enum Message {
     Sample(usize, f64),         // for sending a sample (f64) from an oscillator (usize) to the UI
+    Quit(),                     // for sending exit signal between threads
 }
 
 pub struct Channel {
