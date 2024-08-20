9f21ed6929cb42abfc47d7fb290a3d069533f2d6
Sloane Vaznova <sloane.vaznova@gmail.com>
2024-08-20 18:00:01 -0500

    Polish UI.
    This commit:
    * Centers text boxes under knobs.
    * Centers most knobs in their respective containers.
    * Cleans the spacing and layout of UI elements.
    * Darkens background colors.
    * Builds with `fluent-dark` style rather than `cupertino-dark`.

--- build.rs
+++ build.rs
@@ -2,6 +2,6 @@ fn main() {
     slint_build::compile_with_config(
         "src/ui/app.slint",
         slint_build::CompilerConfiguration::new()
-            .with_style("cupertino-dark".into())
+            .with_style("fluent-dark".into())
     ).unwrap();
 }
